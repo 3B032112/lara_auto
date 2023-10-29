373327d25ade4600b171daedd160861e5316de04
3B032112 <rjoeyic@gmail.com>
2023-10-29 22:44:55 +0800

    實作monolog套件

--- public/index.php
+++ public/index.php
@@ -18,3 +18,15 @@ $hello = new Demo\HelloWorld();
 use Demo\HelloWorld as World;
 
 $world = new World();
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('WISD');
+$log->pushHandler(new StreamHandler('../log/my.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
