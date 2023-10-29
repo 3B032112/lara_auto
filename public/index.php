cefb176069c032017400d1b7ad7f9924657972c1
3B032112 <rjoeyic@gmail.com>
2023-10-29 23:06:04 +0800

    實作Carbon\Carbon套件

--- public/index.php
+++ public/index.php
@@ -7,17 +7,21 @@ use Demo\Hello\Lara;
 use Demo\Hello\Someone;
 
 $lara = new Lara();
+printf("<br>");
 $vincent = new Someone('Vincent');
-
+printf("<br>");
 //passive import namespace
 $mary = new \Demo\Hello\Someone('Mary');
+printf("<br>");
 $john = new Demo\Hello\Someone('John');
+printf("<br>");
 $hello = new Demo\HelloWorld();
-
+printf("<br>");
 //類別別名
 use Demo\HelloWorld as World;
 
 $world = new World();
+printf("<br>");
 
 use Monolog\Level;
 use Monolog\Logger;
@@ -30,3 +34,9 @@ $log->pushHandler(new StreamHandler('../log/my.log', Level::Warning));
 // add records to the log
 $log->warning('Foo');
 $log->error('Bar');
+
+
+use Carbon\Carbon;
+
+printf("現在時間: %s", Carbon::now());
+printf("<br>");
